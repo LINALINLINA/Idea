1dd8a2dac26c963ce219388ea7a6049826f21fad
LINALINLINA <lina.lin@perficient.com>
2018-12-07 18:06:59 +0800

    add css

--- src/component/slider.js
+++ src/component/slider.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import img1 from '../static/img/1.jpg';
+//import img1 from '../static/img/1.jpg';
 import img2 from '../static/img/2.jpg';
+import style from './slider.scss';
 
 export default class Slider extends Component {
     constructor(props) {
         super(props);
-        this.state = {data: {imgArrary: [img1, img2]}};
+        this.state = {data: {imgArrary: [img2]}};
     }
     render() {
         console.log(this.state);
         
         return(
-            <div>
+            <div className= {style.container}>
                 <ul>
                     {this.state.data.imgArrary.map((item)=>(
                         <li key={item}><img src={item} alt={item}/></li>
